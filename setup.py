2fce3d6bf809895e5d407c3fef80d82fa0fdfec8
John Lin <johnlinp@gmail.com>
2019-01-20 19:54:42 +0800

    Add long_description in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+import os
 from setuptools import setup
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='squirrel-bookmarks',
     version='0.1.0',
     description='Bookmark system between development branches.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='johnlinp',
     author_email='johnlinp@gmail.com',
     url='https://github.com/johnlinp/squirrel-bookmarks',
