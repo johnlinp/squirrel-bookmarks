87b184cbc6b191af96954144bb061dec41033952
John Lin <johnlinp@gmail.com>
2019-01-26 10:54:03 +0800

    Support Ubuntu

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 import os
+import platform
 from setuptools import setup
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+
+data_files = [
+    ('etc/bash_completion.d', ['etc/bash_completion.d/sq', 'etc/bash_completion.d/squirrel']),
+    ('etc/profile.d', ['etc/profile.d/sq.sh']),
+]
+
+if platform.system() == 'Linux':
+    import lsb_release
+    lsb_info = lsb_release.get_lsb_information()
+    if lsb_info['ID'] == 'Ubuntu':
+        extra_data_files = [('/{}'.format(data_file[0]), data_file[1]) for data_file in data_files]
+        data_files = data_files + extra_data_files
 
 setup(name='squirrel-bookmarks',
     version='0.1.5',
@@ -19,8 +32,5 @@ setup(name='squirrel-bookmarks',
         'bin/squirrel',
         'bin/sq',
     ],
-    data_files=[
-        ('etc/bash_completion.d', ['etc/bash_completion.d/sq', 'etc/bash_completion.d/squirrel']),
-        ('etc/profile.d', ['etc/profile.d/sq.sh']),
-    ]
+    data_files=data_files
 )
