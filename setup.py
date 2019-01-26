e8310debd6b52aa777ff18fd23cb13d17f5a4bfa
John Lin <johnlinp@gmail.com>
2019-01-26 11:00:06 +0800

    Bump version to 0.1.6

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if platform.system() == 'Linux':
         data_files = data_files + extra_data_files
 
 setup(name='squirrel-bookmarks',
-    version='0.1.5',
+    version='0.1.6',
     description='Bookmark system between development branches.',
     long_description=long_description,
     long_description_content_type='text/markdown',
